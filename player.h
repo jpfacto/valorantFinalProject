55283c3eecac65aef602e1e1ee5cae224ae4a789
jpfacto <jan06.janelle@gmail.com>
2026-06-29 12:58:38 +0800

    Add Player files

---
+++ player.h
@@ -0,0 +1,56 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <thread>
+#include <barrier>
+#include <mutex>
+
+using namespace std;
+
+class Agent {
+public:
+    string name;
+
+    Agent(string n = "");
+};
+
+class Player {
+public:
+    string name;
+    string team;
+    string agent;
+    bool connected;
+    bool selected;
+
+    Player(string n = "");
+};
+
+class Team {
+public:
+    string name;
+    vector<Player*> players;
+
+    Team(string n = "");
+};
+
+struct Match {
+    Team* team1;
+    Team* team2;
+};
+
+vector<Team> defaultTeams();
+void createTeams(vector<Team>& teams, int additionalTeams);
+vector<Player> createPlayers(int totalPlayers);
+void teamAssignment(vector<Player>& players, vector<Team>& teams);
+
+vector<Agent> createAgents();
+
+void playerConnection(Player& player, barrier<>& sync);
+void agentSelection(Player& player, vector<Agent>& availableAgents, barrier<>& sync);
+
+void playerCode();
+
+#endif
